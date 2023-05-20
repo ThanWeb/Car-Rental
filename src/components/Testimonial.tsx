a1e0c9b2300c70706b25df6b77bec5c722c9c2e8
Hans <classsickpeople@gmail.com>
2023-05-20 11:38:50 +0800

    Menambahkan carousel pada landing

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,4 +1,7 @@
 import type { ReactElement } from 'react'
+import { useState } from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css'
 
 interface TestimonialItem {
     name: string
@@ -32,10 +35,54 @@ const Testimonial = (): ReactElement => {
             testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod'
         }
     ]
+    const testimonialItemsElement: any[] = []
     const stars: any[] = []
+    const [indexCarousel, setIndexCarousel] = useState(0)
 
     for (let i = 0; i < 5; i++) {
         stars.push(<img key={i} src='images/icons/star.svg' alt='Star' title='Star'/>)
+    }
+
+    for (let i = 0; i < testimonialItems.length; i++) {
+        testimonialItemsElement.push(
+            <div key={i} className='mx-4 py-10 px-8 flex gap-x-12 testimonial-item'>
+                <div className='flex items-center w-full'>
+                    <img src={`images/landing/${testimonialItems[i].image}`} alt={testimonialItems[i].name} title={testimonialItems[i].name} className='w-full'/>
+                </div>
+                <div className='flex flex-col gap-y-2'>
+                    <div className='flex'>
+                        {stars}
+                    </div>
+                    <div>
+                        <p className='font-light text-sm mb-2'>{testimonialItems[i].testimonial}</p>
+                        <span className='font-normal text-sm'>{`${testimonialItems[i].name} ${testimonialItems[i].age}, ${testimonialItems[i].address}`}</span>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const responsiveCarousel = {
+        0: {
+            items: 3
+        }
+    }
+
+    const changeCarouselIndex = (value: number): void => {
+        if (value === -1) {
+            if (indexCarousel === 0) {
+                setIndexCarousel(2)
+            } else {
+                setIndexCarousel(indexCarousel + value)
+            }
+        } else {
+            if (indexCarousel === 2) {
+                setIndexCarousel(0)
+            } else {
+                setIndexCarousel(indexCarousel + value)
+            }
+        }
+        // console.log(value)
     }
 
     return (
@@ -43,24 +90,15 @@ const Testimonial = (): ReactElement => {
             <h2 className='text-2xl leading-9 font-bold mb-4 text-center'>Testimonial</h2>
             <p className='font-light text-sm mb-10 text-center'>Berbagai review positif dari para pelanggan kami</p>
             <div className='flex gap-x-8'>
-                {
-                    testimonialItems.map((item, index) =>
-                        <div key={index} className='py-10 px-8 flex gap-x-12 testimonial-item'>
-                            <div className='flex items-center w-full'>
-                                <img src={`images/landing/${item.image}`} alt={item.name} title={item.name} className='w-full'/>
-                            </div>
-                            <div className='flex flex-col gap-y-2'>
-                                <div className='flex'>
-                                    {stars}
-                                </div>
-                                <div>
-                                    <p className='font-light text-sm mb-2'>{item.testimonial}</p>
-                                    <span className='font-normal text-sm'>{`${item.name} ${item.age}, ${item.address}`}</span>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
+                <AliceCarousel activeIndex={indexCarousel} mouseTracking items={testimonialItemsElement} infinite={true} responsive={responsiveCarousel} disableDotsControls={true} disableButtonsControls={true} />
+            </div>
+            <div className='mt-6 flex gap-x-6 w-full justify-center'>
+                <button type='button' className='testimonial-btn rounded-full border flex p-1 justify-center items-center' onClick={() => { changeCarouselIndex(-1) }}>
+                    <img src='images/icons/chevron_left.svg' alt='Left' title='Left' />
+                </button>
+                <button type='button' className='testimonial-btn rounded-full border flex p-1 justify-center items-center' onClick={() => { changeCarouselIndex(1) }}>
+                    <img src='images/icons/chevron_right.svg' alt='Right' title='Right' />
+                </button>
             </div>
         </div>
     )
